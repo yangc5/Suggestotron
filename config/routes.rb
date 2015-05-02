1eb7bb078f32cbd068ffccf28a4b007d7402077f
Chen Yang <yangc5@mail.uc.edu>
2015-05-02 14:28:37 -0500

    added downvote button

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   resources :topics do
       member do
         post 'upvote'
+        post 'downvote'
       end
   end
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
